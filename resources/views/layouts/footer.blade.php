32a126bb6a9fc4e2a975739a2c0d5f00d5b64db1
dandelior <jberrioscifuentes@gmail.com>
2020-08-10 16:16:14 -0400

    swiper fixed

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -3,7 +3,8 @@
 </footer>
 
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://unpkg.com/swiper/js/swiper.min.js"></script>
+{{-- <script src="https://unpkg.com/swiper/js/swiper.min.js"></script> --}}
+<script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
 <script src="{{asset('/js/app.js')}}"></script>
 </body>
 </html>
--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -5,7 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Todo Brillo</title>
     <link href="https://fonts.googleapis.com/css?family=Noto+Sans+SC:400,700,900&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css">
+    {{-- <link rel="stylesheet" href="https://unpkg.com/swiper/css/swiper.min.css"> --}}
+    <link rel="stylesheet" href="https://unpkg.com/swiper/swiper-bundle.min.css">
     <link rel="stylesheet" href="{{asset('/css/app.css')}}">
     <meta property="og:title" content="Todo Brillo"/>
     <!-- <meta property="og:image" content=""/> -->
